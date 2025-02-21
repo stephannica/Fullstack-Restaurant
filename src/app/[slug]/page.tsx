edfcd221029cf41150aced7c177da8a2e0f941ca
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-02-21 16:43:02 -0300

    feat: add initial restaurant page and menu page

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+import ConsumptionMethodOption from "./components/consumption-method-option";
+
+interface RestaurantPageProps {
+  params: Promise<{ slug: string }>;
+}
+
+const RestaurantPage = async ({ params }: RestaurantPageProps) => {
+  const { slug } = await params;
+  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+  if (!restaurant) {
+    return notFound();
+  }
+  return (
+    <div className="flex h-screen flex-col items-center justify-center px-6 pt-24">
+      {/* Logo e nome */}
+      <div className="flex flex-col items-center gap-2">
+        <Image
+          src={restaurant.avatarImageUrl}
+          alt={restaurant.name}
+          width={82}
+          height={82}
+        />
+        <h2 className="font-semibold">{restaurant.name}</h2>
+      </div>
+      {/* Textos */}
+      <div className="space-y-2 pt-24 text-center">
+        <h3 className="text-2xl font-semibold">Seja bem-vindo!</h3>
+        <p className="opacity-55">
+          Escolha como prefere aproveitar sua refeição. Estamos aqui para
+          oferecer praticidade e sabor em cada detalhe!
+        </p>
+      </div>
+      {/* Menu */}
+      <div className="grid grid-cols-2 gap-4 pt-14">
+        <ConsumptionMethodOption
+          slug={slug}
+          imageUrl="/dineIn.png"
+          imageAlt="Para comer aqui"
+          buttonText="Para comer aqui"
+          option="DINE_IN"
+        />
+        <ConsumptionMethodOption
+          slug={slug}
+          imageUrl="/takeAway.png"
+          imageAlt="Para levar"
+          buttonText="Para levar"
+          option="TAKEAWAY"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantPage;
