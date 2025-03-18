f9dc8d4b7e438aeba47e6ce96f92e7886aa627fc
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-03-18 15:41:39 -0300

    feat: insure same product can be added more than once to cart

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -30,8 +30,26 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
   };
 
   const addProduct = (product: CartProduct) => {
-    setProducts((prev) => [...prev, product]);
+    //verificar se o produto já está no carrinho
+    //se estiver, aumente a quantidade
+    //se não estiver, adicione o produto
+    const productIsAlreadyOnTheCart = products.some((prevProduct) => prevProduct.id === product.id);
+    if (!productIsAlreadyOnTheCart){
+        return setProducts((prev) => [...prev, product]);
+    }
+    setProducts((prevProducts) => {
+        return prevProducts.map((prevProduct) => {
+            if (prevProduct.id === product.id) {
+                return {
+                    ...prevProduct,
+                    quantity: prevProduct.quantity + product.quantity,
+                };
+            }
+            return prevProduct;
+        });
+    });
   }
+  
   return (
     <CartContext.Provider
       value={{
