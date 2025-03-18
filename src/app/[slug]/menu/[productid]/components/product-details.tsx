b5d0a3acd3f12d6c2aff4b0aef2640ca48773436
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-03-18 15:27:49 -0300

    feat: add initial add to cart function to cart context

--- src/app/[slug]/menu/[productid]/components/product-details.tsx
+++ src/app/[slug]/menu/[productid]/components/product-details.tsx
@@ -26,7 +26,7 @@ interface ProductDetailsProps {
 }
 
 const ProductDetails = ({ product }: ProductDetailsProps) => {
-  const { toggleCart } = useContext(CartContext);
+  const { toggleCart, addProduct } = useContext(CartContext);
   const [quantity, setQuantity] = useState<number>(1);
 
   const handleDecreaseQuantity = () => {
@@ -44,6 +44,10 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
   };
 
   const handleAddToCart = () => {
+    addProduct({
+      ...product,
+      quantity,
+    });
     toggleCart();
   };
   return (
--- src/app/[slug]/menu/components/cart-sheet.tsx
+++ src/app/[slug]/menu/components/cart-sheet.tsx
@@ -6,7 +6,7 @@ import { CartContext } from "../contexts/cart";
 
 
 const CartSheet = () => {
-    const { isOpen, toggleCart } = useContext(CartContext);
+    const { isOpen, toggleCart, products } = useContext(CartContext);
     return ( 
         <Sheet open={isOpen} onOpenChange={toggleCart}> 
         <SheetContent>
@@ -17,6 +17,9 @@ const CartSheet = () => {
               and remove your data from our servers.
             </SheetDescription>
           </SheetHeader>
+          {products.map((product) => (
+            <h1 key={product.id}> {product.name} - {product.quantity} </h1>
+          ))}
         </SheetContent>
       </Sheet>
      );
