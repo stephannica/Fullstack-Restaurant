d173bf478834fed2e2c6771a0b51c03418a99a7f
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-03-20 14:33:46 -0300

    feat: ensure cart product quantity can be removed

--- src/app/[slug]/menu/components/cart-product-item.tsx
+++ src/app/[slug]/menu/components/cart-product-item.tsx
@@ -12,7 +12,7 @@ interface CartProductItemProps {
 }
 
 const CartProductItem = ({product}: CartProductItemProps) => {
-    const {decreaseProductQuantity, increaseProductQuantity} = useContext(CartContext);
+    const {decreaseProductQuantity, increaseProductQuantity, removeProduct} = useContext(CartContext);
     return ( 
         <div className="flex items-center justify-between">
             {/* ESQUERDA */}
@@ -38,7 +38,7 @@ const CartProductItem = ({product}: CartProductItemProps) => {
             </div>
 
             {/* BOTÃO DE DELEÇÃO */}
-            <Button variant="outline" className="h-7 w-7 rounded-lg">
+            <Button variant="outline" className="h-7 w-7 rounded-lg" onClick={() => removeProduct(product.id)}>
                 <TrashIcon />
             </Button>
         </div>
