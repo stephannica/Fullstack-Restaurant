e714ac71f758c57d8191bf7466a2fe58a7388e0b
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-03-20 14:09:47 -0300

    feat: ensure cart product quantity can be decreased

--- src/app/[slug]/menu/components/cart-product-item.tsx
+++ src/app/[slug]/menu/components/cart-product-item.tsx
@@ -1,33 +1,36 @@
 import { ChevronLeftIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import Image from "next/image";
+import { useContext } from "react";
 
 import { Button } from "@/components/ui/button";
 import { formatCurrency } from "@/helpers/format-currency";
 
-import { CartProduct } from "../contexts/cart";
+import { CartContext, CartProduct } from "../contexts/cart";
 
 interface CartProductItemProps {
     product: CartProduct;
 }
 
 const CartProductItem = ({product}: CartProductItemProps) => {
+    const {decreaseProductQuantity} = useContext(CartContext);
     return ( 
         <div className="flex items-center justify-between">
             {/* ESQUERDA */}
             <div className="flex items-center gap-3">
-                <div className="relative h-20 w-20 bg-gray-100 rounded-lg">
+                <div className="relative h-20 w-20 bg-gray-100 rounded-xl">
                     <Image src={product.imageUrl} alt={product.name} fill/>
                 </div>
 
                 <div className="space-y-1">
                     <p className="text-xs max-w-[90%] truncate text-ellipsis">{product.name}</p>
                     <p className="text-sm font-semibold">{formatCurrency(product.price)}</p>
+                    {/* QUANTIDADE */}
                     <div className="flex items-center gap-1 text-center">
-                        <Button variant="outline" className="w-7 h-7 rounded-lg">
+                        <Button variant="outline" className="h-7 w-7 rounded-lg" onClick={() => decreaseProductQuantity(product.id)}>
                             <ChevronLeftIcon />
                         </Button>
                         <p className="w-7 text-xs">{product.quantity}</p>
-                        <Button variant="destructive" className="w-7 h-7 rounded-lg">
+                        <Button variant="destructive" className="h-7 w-7 rounded-lg">
                             <ChevronRightIcon />
                         </Button>
                     </div>
@@ -35,11 +38,11 @@ const CartProductItem = ({product}: CartProductItemProps) => {
             </div>
 
             {/* BOTÃO DE DELEÇÃO */}
-            <Button variant="outline" className="w-7 h-7 rounded-lg">
+            <Button variant="outline" className="h-7 w-7 rounded-lg">
                 <TrashIcon />
             </Button>
         </div>
-     );
+    );
 }
  
 export default CartProductItem;
--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -12,6 +12,7 @@ export interface ICartContext {
   products: CartProduct[];
   toggleCart: () => void;
   addProduct: (product: CartProduct) => void;
+  decreaseProductQuantity: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -19,6 +20,7 @@ export const CartContext = createContext<ICartContext>({
   products: [],
   toggleCart: () => {},
   addProduct: () => {},
+  decreaseProductQuantity: () => {},
 });
 
 export const CartProvider = ({ children }: { children: ReactNode }) => {
@@ -50,13 +52,28 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
     });
   }
   
+  const decreaseProductQuantity = (productId: string) => {
+    setProducts(prevProducts => {
+      return prevProducts.map(prevProduct => {
+        if (prevProduct.id !== productId) {
+          return prevProduct;
+        }
+        if (prevProduct.quantity === 1) {
+          return prevProduct;
+        }
+          return {...prevProduct, quantity: prevProduct.quantity - 1};
+      })
+    })
+  }
+
   return (
     <CartContext.Provider
       value={{
         isOpen,
         products,
         toggleCart,
-        addProduct
+        addProduct,
+        decreaseProductQuantity,
       }}
     >
       {children}
