7a0d301b1ed9a7d82d714ffff69ae8fd24e6bb90
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-03-27 12:04:59 -0300

    chore: restore removed file and create button to redirect to official link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
+import Link from "next/link";
+
 const HomePage = () => {
-  return <h1>Olá</h1>;
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="space-y-4 rounded-lg bg-gray-100 p-6 text-center shadow-lg">
+        <h1 className="text-lg">
+          Olá, caso você tenha vindo para este link, para acessar o restaurante,
+          digite na URL <span className="font-bold">/fsw-donalds</span>.
+        </h1>
+        <h2 className="text-lg font-semibold">Ou clique aqui ↓</h2>
+        <div>
+            <Link
+            href="https://fullstack-restaurant-two.vercel.app/fsw-donalds"
+            className="inline-block rounded-full border border-gray-300 bg-white px-5 py-2.5 text-gray-900 transition hover:bg-gray-500 hover:text-white"
+            >
+            Acessar Restaurante
+            </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
