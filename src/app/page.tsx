1a466e834b9b21d5b81b3f1cb6e9bd946ab5523b
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-03-27 15:48:33 -0300

    chore: add info about initial phone-only layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { InfoIcon } from "lucide-react";
 import Link from "next/link";
 
 const HomePage = () => {
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="space-y-4 rounded-lg bg-gray-100 p-6 text-center shadow-lg">
+          <InfoIcon className="m-auto"/> 
+        <h1 className="font-bold" >
+          Este projeto está inicialmente em resolução para celular 
+        </h1>
         <h1 className="text-lg">
           Olá, caso você tenha vindo para este link, para acessar o restaurante,
           digite na URL <span className="font-bold">/fsw-donalds</span>.
