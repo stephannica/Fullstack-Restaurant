e20ee9347d13bf273cd1ace5008c717ad820769f
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-03-28 13:32:41 -0300

    style: fix position of last product above total resume

--- src/app/[slug]/menu/components/categories.tsx
+++ src/app/[slug]/menu/components/categories.tsx
@@ -43,8 +43,8 @@ const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
   };
   return (
     <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl bg-white">
-      <div className="p-5 ">
-        <div className="flex items-center gap-3 ">
+      <div className="p-5">
+        <div className="flex items-center gap-3">
           <Image
             src={restaurant.avatarImageUrl}
             alt={restaurant.name}
@@ -59,7 +59,7 @@ const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
             <Image src="/rating.png" alt="rating" width={55} height={55} />
           </div>
         </div>
-        
+
         <div className="mt-3 flex items-center gap-1 text-xs text-green-500">
           <ClockIcon size={12} />
           <p>Aberto!</p>
@@ -84,7 +84,9 @@ const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
       </ScrollArea>
 
       <h3 className="px-5 pt-2 font-semibold">{selectedCategory.name}</h3>
-      <Products products={selectedCategory.products} />
+      <div className="pb-14">
+        <Products products={selectedCategory.products} />
+      </div>
       {products.length > 0 && (
         <div className="fixed bottom-0 left-0 right-0 flex w-full items-center justify-between border-t bg-white px-5 py-3 shadow-[0_-35px_35px_rgba(0,0,0,0.10)]">
           <div>
@@ -96,7 +98,9 @@ const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
               </span>
             </p>
           </div>
-          <Button onClick={toggleCart} variant="destructive">Ver sacola</Button>
+          <Button onClick={toggleCart} variant="destructive">
+            Ver sacola
+          </Button>
           <CartSheet />
         </div>
       )}
